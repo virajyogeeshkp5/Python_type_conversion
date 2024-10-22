507a7c5c0e805ff1b5eb1f207a1c20612c8db2a8
YOGEESH K P <yogeesh793@gmail.com>
2024-10-22 19:06:32 +0530

    Add files via upload

---
+++ Python_type_conversion.py
@@ -0,0 +1,91 @@
+# Python Type Conversion
+a=10
+b=2.3
+print(a+b)
+
+a=4
+b=7.1
+c=2+4j
+print(a+b+c)
+
+# float converted in to int
+a=15  # int
+b=100.23 # float
+s=int(b)
+print(a+s)
+
+# String converted in to int
+a=10 # int
+b='25' # str
+s=int(b)
+print(a+s)
+
+# bool converted float
+f=float(True)
+print(f)
+
+# float converted in to complex
+c=complex(2.05)
+print(c)
+
+# int converted in to bool
+b=bool(6)
+print(b)
+
+# char converted in to ord
+o=ord('a')
+print(o)
+
+# ord converted in to chr
+chr=chr(97)
+print(chr)
+
+# string converted in to tuple
+tuple=tuple('bit')
+print(tuple)
+
+# converted list
+list=list([1,2,3])
+print(list)
+
+# converted set
+s=set([1,2,3])
+print(s)
+
+# bool converted in to int
+j=int(True)
+print(j)
+
+# bool converted in to int
+k=float(True)
+print(k)
+
+# string converted in to float
+x='45'
+y=25+float(x)
+print(y)
+
+# float converted in to complex
+v=complex(23.01)
+print(v)
+
+# int converted in to string
+p=65.02
+u=str(p)
+print(u)
+
+# string converted in to bool
+j=bool('mouse')
+print(j)
+
+# float converted in to bool
+l=bool(0.0)
+print(l)
+
+# char converted in to ord
+r=ord('A')
+print(r)
+
+x=[python]
+u=str(x)
+print(u)
